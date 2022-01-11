0842559b18530ef97a3b042b9cbf9194aaacaf43
olyop <oliver.plummer@outlook.com>
2022-01-11 13:24:10 +1100

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
+import {
+	FC,
+	ReactNode,
+	createElement,
+	CSSProperties,
+	ButtonHTMLAttributes,
+} from "react"
+
 import Image from "@oly_op/react-image"
 import { createBEM, BEMInput } from "@oly_op/bem"
-import { createElement, FC, Fragment, ButtonHTMLAttributes, ReactNode } from "react"
 
 import "./index.scss"
+
+const isString =
+	(value: unknown): value is string =>
+		typeof value === "string"
 
 const bem =
 	createBEM("Button")
@@ -15,10 +26,15 @@ const Button: FC<ButtonPropTypes> = ({
 	onClick,
 	className,
 	rightIcon,
+	spanStyle,
+	iconStyle,
+	textStyle,
 	spanClassName,
 	iconClassName,
 	textClassName,
+	leftIconStyle,
 	imageClassName,
+	rightIconStyle,
 	rightIconClassName,
 	transparent = false,
 	...props
@@ -26,10 +42,7 @@ const Button: FC<ButtonPropTypes> = ({
 	<button
 		type="button"
 		onClick={onClick}
-		title={title || (
-			typeof text === "string" ?
-				text : undefined
-		)}
+		title={title || (isString(text) ? text : undefined)}
 		{...props}
 		className={bem(
 			className,
@@ -38,75 +51,102 @@ const Button: FC<ButtonPropTypes> = ({
 			"",
 			"FlexRowCenter Rounded",
 		)}
-		children={(
-			<Fragment>
-				{icon && (
-					<i
-						children={icon}
-						className={bem(
-							iconClassName,
-							spanClassName,
-							"icon-left",
-							"icon",
-							{ ignore: true, className: "material-icons" },
-						)}
-					/>
+	>
+		{icon && (
+			<i
+				children={icon}
+				style={{
+					...spanStyle,
+					...iconStyle,
+					...leftIconStyle,
+				}}
+				className={bem(
+					iconClassName,
+					spanClassName,
+					"icon-left",
+					"icon",
+					{ ignore: true, className: "material-icons" },
 				)}
-				{image && (
-					<Image
-						url={image}
-						className={bem(
-							imageClassName,
-							spanClassName,
-							"image",
-							"icon",
-						)}
-					/>
-				)}
-				{text && (
-					<span
-						children={text}
-						className={bem(
-							textClassName,
-							spanClassName,
-							"text",
-							"UpperCase",
-						)}
-					/>
-				)}
-				{rightIcon && (
-					<i
-						children={rightIcon}
-						className={bem(
-							rightIconClassName,
-							spanClassName,
-							"icon-right",
-							"icon",
-							{ ignore: true, className: "material-icons" },
-						)}
-					/>
-				)}
-			</Fragment>
+			/>
 		)}
-	/>
+		{image && (
+			<Image
+				url={image}
+				className={bem(
+					imageClassName,
+					spanClassName,
+					"image",
+					"icon",
+				)}
+			/>
+		)}
+		{text && (
+			<span
+				children={text}
+				style={{
+					...spanStyle,
+					...textStyle,
+				}}
+				className={bem(
+					textClassName,
+					spanClassName,
+					"text",
+					"UpperCase",
+				)}
+			/>
+		)}
+		{rightIcon && (
+			<i
+				children={rightIcon}
+				style={{
+					...spanStyle,
+					...iconStyle,
+					...rightIconStyle,
+				}}
+				className={bem(
+					rightIconClassName,
+					spanClassName,
+					"icon-right",
+					"icon",
+					{ ignore: true, className: "material-icons" },
+				)}
+			/>
+		)}
+	</button>
 )
 
 type HTMLButtonPropTypes =
-	Omit<ButtonHTMLAttributes<HTMLButtonElement>, "className" | "title">
+	Omit<ButtonHTMLAttributes<HTMLButtonElement>, "style" | "className" | "title" | "onClick">
 
-export interface ButtonPropTypes extends HTMLButtonPropTypes {
-	icon?: string,
-	image?: string,
-	title?: string,
-	text?: ReactNode,
-	rightIcon?: string,
+interface ButtonStylePropTypes {
+	style?: CSSProperties,
+	textStyle?: CSSProperties,
+	iconStyle?: CSSProperties,
+	spanStyle?: CSSProperties,
+	leftIconStyle?: CSSProperties,
+	rightIconStyle?: CSSProperties,
+}
+
+interface ButtonClassNamePropTypes {
 	className?: BEMInput,
-	transparent?: boolean,
 	spanClassName?: BEMInput,
 	iconClassName?: BEMInput,
 	textClassName?: BEMInput,
 	imageClassName?: BEMInput,
 	rightIconClassName?: BEMInput,
+}
+
+export interface ButtonPropTypes
+	extends
+	HTMLButtonPropTypes,
+	ButtonStylePropTypes,
+	ButtonClassNamePropTypes {
+	icon?: string,
+	image?: string,
+	title?: string,
+	text?: ReactNode,
+	rightIcon?: string,
+	transparent?: boolean,
 	onClick?: () => void | Promise<void>,
 }
 
