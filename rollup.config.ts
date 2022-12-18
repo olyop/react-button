a021d3c815f13dcd54486a3d206b84b2bc7e7211
Oliver Plummer <oliver.plummer@outlook.com>
2022-12-18 12:13:20 +1100

    Update

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,29 +1,29 @@
-import jsx from "acorn-jsx"
-import sass from "rollup-plugin-sass"
-import { RollupOptions } from "rollup"
-import typescript from "@rollup/plugin-typescript"
+import typescript from "@rollup/plugin-typescript";
+import jsx from "acorn-jsx";
+import { RollupOptions } from "rollup";
+import sass from "rollup-plugin-sass";
 
-type AcornInjectPlugins =
-	ReturnType<typeof jsx>
+type AcornInjectPlugins = ReturnType<typeof jsx>;
 
 declare module "rollup" {
 	interface RollupOptions {
-		acornInjectPlugins?: AcornInjectPlugins[],
+		acornInjectPlugins?: AcornInjectPlugins[];
 	}
 }
 
 const options: RollupOptions = {
 	input: "src/index.tsx",
-	output: [{
-		format: "esm",
-		strict: false,
-		exports: "named",
-		file: "build/index.js",
-	}],
-	acornInjectPlugins: [
-		jsx(),
+	output: [
+		{
+			format: "esm",
+			strict: false,
+			exports: "named",
+			file: "build/index.js",
+		},
 	],
+	acornInjectPlugins: [jsx()],
 	plugins: [
+		// @ts-ignore
 		sass({
 			output: "build/index.css",
 			options: {
@@ -34,11 +34,7 @@ const options: RollupOptions = {
 			tsconfig: "./tsconfig.rollup.json",
 		}),
 	],
-	external: [
-		"react",
-		"react-dom",
-		"@oly_op/bem",
-	],
-}
+	external: ["react", "react-dom", "@oly_op/bem"],
+};
 
-export default options
+export default options;
